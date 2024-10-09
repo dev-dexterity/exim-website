b361e912e8fee224456f5282b7e93ba613ba67bf
dev-dexterity <omkargaikwad2801@gmail.com>
2024-10-09 15:50:42 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navber/navbar'
 import Hero from './components/Hero/hero'
 import Services from './components/Services/services'
+import Title from '../src/components/Title/title'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       
       <div className="container">
+        <Title subTitle = 'Our Services' title= 'What we Offer'/>
         <Services />
       </div>
     </div>
