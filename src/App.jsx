532c9deb96bb41b31805ea8e19484ab18a1f644a
dev-dexterity <omkargaikwad2801@gmail.com>
2024-10-10 00:59:39 +0530

    Created the slideeer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero/hero'
 import Services from './components/Services/services'
 import Title from '../src/components/Title/title'
 import About from '../src/components/About/about'
+import Products from '../src/components/Products/products'
+
+
 
 
 const App = () => {
@@ -11,14 +14,17 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
-      
       <div className="container">
         <Title subTitle = 'Our Services' title= 'What we Offer'/>
         <Services />
         <About />
+        <Title subTitle = 'Our Products' title= 'What we Have'/>
+        <Products />
       </div>
     </div>
   )
 }
+
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+ReactDOM.render(<App />, document.getElementById("app"));
