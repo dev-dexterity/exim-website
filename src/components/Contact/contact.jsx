d8a08dfdd6386ca42219915bb09d66f5d9165ea9
dev-dexterity <omkargaikwad2801@gmail.com>
2024-10-10 02:04:25 +0530

    successfully created a workingform

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,8 +4,37 @@ import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import call_icon from '../../assets/phone-icon.png'
 import loc_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const contact = () => {
+
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "472d8075-09e5-4d1a-a046-984c4c5e3154");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+
   return (
     <div className='contact'>
       <div className="contact-col">
@@ -17,7 +46,18 @@ const contact = () => {
             <li> <img src={loc_icon} alt="" /> Satyam Industrial Estate, Aasmant Apts, Sr.No.1/4, Near, Opp. SNDT College, Pune, Maharashtra 411004, India</li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+            <label>Your Name</label>
+            <input type="text" name='name' placeholder='Enter your Name' required/>
+            <label>Phone Number</label>
+            <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
+            <label>Write your message here</label>
+            <textarea name="message" rows="6" placeholder='Enter your Message' required></textarea>
+            <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /> </button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   )
 }
